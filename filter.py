8505df8fb8e966e9ada5d6cec365942e217200db
4l3xd <alessandra.olea@gmail.com>
2020-12-04 21:36:57 -0300

    string pre-processing

--- filter.py
+++ filter.py
@@ -1,27 +1,29 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import xlrd
 
-formulario = pd.read_excel('cestas_digitais.xlsx', sheet_name='Respostas ao formulário',
+formulario = pd.read_excel('cestas_digitais.xlsx',sheet_name='Respostas ao formulario',
     usecols=['Sexo', 'Data de nascimento', 'Idade',	'Filhos', 'Primeiro filho',
-    'Estado civil', 'Raça/Etnia', 'Escolaridade', 'Endereço', 'Bairro', 
-    'Propriedade', 'Valor aluguel', 'Energia elétrica', 'Água encanada',
-    'Saneamento básico', 'Rua asfaltada', 'Telha', 'Piso', 'Teto',
-    'Parede interna', 'Parede externa', 'Banheiros', 'Cômodos (sem banheiro)',
-    'Terreno murado', 'NIS', 'Bolsa Família', 'Valor Bolsa Família',
-    'Atividade remunerada', 'Local trabalho', 'Carteira assinada', 'Auxílio Emergencial',
-    'Valor Auxílio Emergencial', 'Aposentadoria', 'Valor aposentadoria',
-    'Pensão alimentícia', 'Valor pensão alimentícia', 'Auxilio reclusão',
-    'Valor auxilio reclusão', 'Trabalhadores residentes', 'Qtdd trabalhadores residentes',
-    'Empresas contratantes', 'Renda bruta família', 'Beneficiados da renda',
-    'Saúde entrevistadx', 'Doença entrevistadx', 'Deficiência entrevistadx',
-    'Tipo deficiência entrevistadx', 'Especificação doença', 'Saúde família',
-    'Pessoas doentes', 'Deficientes família', 'Tipo deficiência família',
-    'Especificação deficiência', 'Tipo doença família', 'Crianças de 0 a 6 anos',
-    'Crianças de 7 a 12 anos', 'Adolescentes de 13 a 18 anos', 'Parentesco',
-    'Idade criança', 'Escola', 'Período integral', 'Projetos',
+    'Estado civil', 'Raca/Etnia', 'Escolaridade', 'Endereco', 'Bairro', 
+    'Propriedade', 'Valor aluguel', 'Energia eletrica', 'Agua encanada',
+    'Saneamento basico', 'Rua asfaltada', 'Telha', 'Piso', 'Teto',
+    'Parede interna', 'Parede externa', 'Banheiros', 'Comodos (sem banheiro)',
+    'Terreno murado', 'NIS', 'Bolsa Familia', 'Valor Bolsa Familia',
+    'Atividade remunerada', 'Local trabalho', 'Carteira assinada', 'Auxilio Emergencial',
+    'Valor Auxilio Emergencial', 'Aposentadoria', 'Valor aposentadoria',
+    'Pensao alimenticia', 'Valor pensao alimenticia', 'Auxilio reclusao',
+    'Valor auxilio reclusao', 'Trabalhadores residentes', 'Qtdd trabalhadores residentes',
+    'Empresas contratantes', 'Renda bruta familia', 'Beneficiados da renda',
+    'Saude entrevistadx', 'Doenca entrevistadx', 'Deficiencia entrevistadx',
+    'Tipo deficiencia entrevistadx', 'Especificacao doenca', 'Saude familia',
+    'Pessoas doentes', 'Deficientes familia', 'Tipo deficiencia familia',
+    'Especificacao deficiencia', 'Tipo doenca familia', 'Criancas de 0 a 6 anos',
+    'Criancas de 7 a 12 anos', 'Adolescentes de 13 a 18 anos', 'Parentesco',
+    'Idade crianca', 'Escola', 'Periodo integral', 'Projetos',
     'Acesso a internet', 'Meio de acesso internet', 'Dispositivos'])
 
 print(formulario)
+
 
 
 
